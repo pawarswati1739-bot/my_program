0346b59bcc7201da457972b6ebbabf307c7ad2e9
Swati Pawar <243333838+pawarswati1739-bot@users.noreply.github.com>
2025-12-12 11:33:44 +0530

    Break and continue
    all programs

---
+++ Q_hw.java
@@ -0,0 +1,13 @@
+package com.breakcontinue;
+
+public class Q_hw {
+  public static void main(String[] args) {
+	for(int i=1; i<=100; i++) {
+		if(i>25  && i<=75 ) {
+			continue;
+		}
+		System.out.println(i);
+
+	}
+}
+}
