3a53981d4582a44ba774161ec7b0424ec6442573
Swati Pawar <243333838+pawarswati1739-bot@users.noreply.github.com>
2025-12-13 12:50:40 +0530

    Enhance TimeConversion with seconds to minutes
    Added functionality to convert seconds to minutes.

--- scanner/TimeConversion.java
+++ scanner/TimeConversion.java
@@ -8,6 +8,10 @@ public static void main(String[] args) {
 	System.out.println("enter a time t =");
 	int  t = sc.nextInt();
 	int sec= t *60;
+	system.out.println("Enter a time in sec =");
+	int second=sc.nextInt();
+	int min= second / 60;
 	System.out.println("Time in min to sec ="+ sec);
 }
 }
+
